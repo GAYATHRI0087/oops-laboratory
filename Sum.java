0dc948a16ff13e993b50889aa243ea0b9e459468
GAYATHRI0087 <gayathri.240087@aids.ritchennai.edu.in>
2025-08-12 18:36:19 +0530

    Sum.java

---
+++ Sum.java
@@ -0,0 +1,91 @@
+//gayathri.p
+//2117240070087
+program title:sum of numbers from 1 to 100
+  test case 1 :
+
+  public class SumFrom1To100 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum from 1 to 100 is: " + sum);
+    }
+}
+OUTPUT:
+Sum from 1 to 100 is: 5050
+
+
+
+TEST CASE 2:
+public class SumFrom1To50 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            sum += i; 
+        }
+
+        System.out.println("Sum from 1 to 50 is: " + sum);
+    }
+}
+
+OUTPUT:
+Sum from 1 to 50 is: 1275
+
+
+
+TEST CASE 3:
+public class SumFrom1To10 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            sum += i; 
+        }
+
+        System.out.println("Sum from 1 to 10 is: " + sum);
+    }
+}
+
+OUTPUT:
+Sum from 1 to 10 is: 55
+
+
+TEST CASE 4 :
+public class SumFrom50To100 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            sum += i; 
+        }
+
+        System.out.println("Sum from 50 to 100 is: " + sum);
+    }
+}
+
+OUTPUT:
+Sum from 50 to 100 is: 3825
+
+
+TEST CASE 5 :
+public class SumFrom1To1 {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            sum += i; 
+        }
+
+        System.out.println("Sum from 1 to 1 is: " + sum);
+    }
+}
+
+OUTPUT:
+Sum from 1 to 1 is: 1
+
+
+
